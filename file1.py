9c6de851b7581ba2534328e09a706a26f9fd5619
Jack Basner <jbasner@brooklynfriends.org>
2022-12-07 18:31:24 -0500

    comments

--- file1.py
+++ file1.py
@@ -5,3 +5,7 @@ import requests
 import matplotlib.pyplot as plt
 
 # returns a dict of covid data for the US
+
+#funtion reads api into json file gets dictionary and returns it
+#fucntion taking in dictionary adding to database
+#dbbrowser
