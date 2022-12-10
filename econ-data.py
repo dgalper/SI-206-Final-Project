1354d30fbe9bfc46df9ed46ebae3d859b3d24591
Jack Basner <jbasner@brooklynfriends.org>
2022-12-10 14:51:49 -0500

    edited econ_data

--- econ-data.py
+++ econ-data.py
@@ -18,3 +18,41 @@ def get_econ_data(code, start, end, limit):
         print('Error during API request')
         return
     return response
+
+def enter_econ_data_into_database(data, table_name):
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    file_name = dir_path + '/' + "all_data.db"
+    conn = sqlite3.connect(file_name)
+    cur = conn.cursor()
+    cur.execute(f'CREATE TABLE IF NOT EXISTS {table_name} (id INTEGER PRIMARY KEY, date TEXT, value REAL)')
+    for observation in data['observations']:
+        cur.execute(f'INSERT INTO {table_name} (date, value) VALUES (?, ?)', (observation['date'], observation['value']))
+    conn.commit()
+
+def clear_table(data_base, table_name):
+    conn = sqlite3.connect(data_base)
+    cur = conn.cursor()
+    cur.execute(f'DELETE FROM {table_name}')
+    conn.commit()
+
+def drop_table(data_base, table_name):
+    conn = sqlite3.connect(data_base)
+    cur = conn.cursor()
+    cur.execute(f'DROP TABLE IF EXISTS {table_name}')
+    conn.commit()
+
+def get_all_data(code, start_year, end_year):
+    for y in range(start_year, end_year+1, 4):
+        data = get_econ_data(code, f'{y}-01-01', f'{y}-12-31', limit=12)
+        enter_econ_data_into_database(data,f'{code}_econ')
+
+if __name__ == "__main__":
+
+    clear_table('all_data.db', 'UNRATE')
+    get_all_data('UNRATE', 1949, 2021)
+
+    clear_table('all_data.db', 'UMCSENT')
+    get_all_data('UMCSENT', 1949, 2021)
+
+    clear_table('all_data.db', 'DFF')
+    get_all_data('DFF', 1949, 2021)
